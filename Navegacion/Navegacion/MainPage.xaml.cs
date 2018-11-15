bb927c3759b828d745c020fba5206182e3be7e07
Samuel David Lopez Gallego <samueldavidlopezgallego@gmail.com>
2018-11-14 19:45:17 -0500

    Navegacion

--- Navegacion/Navegacion/MainPage.xaml.cs
+++ Navegacion/Navegacion/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Navegacion.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,29 @@ using Xamarin.Forms;
 
 namespace Navegacion
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Inicio_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new HomePage());
+            IsPresented = false;
+        }
+
+        private void Contactenos_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ContactoPage());
+            IsPresented = false;
+        }
+
+        private void Acerca_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new AboutPage());
+            IsPresented = false;
+        }
+    }
 }
